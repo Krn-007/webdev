023ffab4e50067f8c4d87f0f270ed3baf7bc742a
Karanbeer <karanbeer.singh@ssipmt.com>
2024-09-20 15:43:23 +0530

    week 4 done

--- js/week4/http/index.js
+++ js/week4/http/index.js
@@ -30,18 +30,33 @@ app.get("/",(req,res)=>{
 })
 app.post("/",(req,res)=>{
     const isHealthy = req.body.isHealthy;
-    users[0].kidneys.push[{
+    users[0].kidneys.push({
         healthy: isHealthy
-    }]
-    res.json[{
+    })
+    res.json({
         msg: "Done"
-    }]
+    })
 })
 app.put("/",(req,res)=>{
-    
+    for (let i = 0; i < users[0].kidneys.length; i++) {
+        users[0].kidneys[i].healthy = true;
+        
+    }
+    res.json({})
 })
 app.delete("/",(req,res)=>{
-    
+    const newKidneys = [];
+    for (let i = 0; i < users[0].kidneys.length; i++) {
+        if(users[0].kidneys[i].healthy){
+            newKidneys.push({
+                healthy: true
+            })
+        }
+    }
+    users[0].kidneys = newKidneys
+    res.json({
+        msg: "done"
+    })
 })
 
 
